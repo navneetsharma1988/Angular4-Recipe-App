0366d39922ca90a6842903fc5cc3d80f123e2ef1
Navneet Sharma <sharma.navneet1988@gmail.com>
2017-08-04 15:16:36 +0530

    Routing added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -32,6 +32,11 @@ export class RecipeService {
     constructor(private slService: ShoppingListService) {
     }
 
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
     getRecipes() {
         return this.recipes.slice();          // return the copy not actual reference
     }
