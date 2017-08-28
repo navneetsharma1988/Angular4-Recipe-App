0db699b2615c7e2519df4c5758cb8793aa2f353f
Navneet Sharma <sharma.navneet1988@gmail.com>
2017-08-28 16:08:23 +0300

    Http Interceptor added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
@@ -9,6 +11,9 @@ import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { AuthGuard } from '../auth/auth-guard.service'
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { AuthInterceptor } from '../shared/auth.interceptor';
+import { LoggingInterceptor } from '../shared/logging.interceptor';
+
 
 @NgModule({
     declarations: [
@@ -29,7 +34,9 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
         RecipeService,
         DataStorageService,
         AuthService,
-        AuthGuard
+        AuthGuard,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true}
     ]
 })
 export class CoreModule {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,29 +1,34 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 import 'rxjs/Rx';
-import {RecipeService} from '../recipes/recipe.service';
-import {Recipe} from '../recipes/recipe.model';
-import {AuthService} from '../auth/auth.service';
 
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-
   constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-    return this.httpClient.put('https://ng-recipe-book-838b7.firebaseio.com/recipes.json?auth=' + token,
-      this.recipeService.getRecipes());
+    // const headers = new HttpHeaders().set('Authorization', 'Bearer afdklasflaldf');
+
+    // return this.httpClient.put('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   params: new HttpParams().set('auth', token)
+    //   // headers: headers
+    // });
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-838b7.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
     const token = this.authService.getToken();
     // this.httpClient.get<Recipe[]>('https://ng-recipe-book-838b7.firebaseio.com/recipes.json?auth=' + token)
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-838b7.firebaseio.com/recipes.json?auth=' + token, {
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-838b7.firebaseio.com/recipes.json', {
       observe: 'body',
       responseType: 'json'
     })
