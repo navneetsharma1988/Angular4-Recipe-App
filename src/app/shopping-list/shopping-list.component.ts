6d7d10a93e7f741cd49f1cc24b83a436bab1ead3
Navneet Sharma <sharma.navneet1988@gmail.com>
2017-08-31 15:31:26 +0300

    Retrieve Shopping List data from NgRx Store

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
 import {Subscription} from 'rxjs/Subscription';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,20 +11,21 @@ import {Subscription} from 'rxjs/Subscription';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  shoppingListState: Observable<{ ingredients: Ingredient[] }>;
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) {
+  constructor(private slService: ShoppingListService,
+              private store: Store<{shoppingList: { ingredients: Ingredient[]}}>) {
   }
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientChanged
+    this.shoppingListState = this.store.select('shoppingList');
+    /*this.subscription = this.slService.ingredientChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
-      );
+      );*/
   }
 
   onEditItem(index: number) {
